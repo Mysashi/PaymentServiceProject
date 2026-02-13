5d55870592be598e8712ef43a6ddea7997e19dc4
Mysashi <plantariy@gmail.com>
2026-02-13 12:07:02 +0500

    First commit

---
+++ OrderGeneral/src/test/java/com/kassa/KassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
